de89ec9da6b67fed77c9b2293af6d4b38a7c1bf5
samjwilson <samueljwilson@yahoo.com>
2018-04-02 22:11:19 -0600

    adding signin authentication

--- src/App.js
+++ src/App.js
@@ -41,6 +41,8 @@ class App extends Component {
     }
   }
 
+
+
 calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     console.log(clarifaiFace);
